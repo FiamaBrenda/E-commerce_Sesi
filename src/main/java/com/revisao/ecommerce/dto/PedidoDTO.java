328ce6043c2b50237991d2b4ccdd550c3e039fd9
lucascorreaa <lucas.lca14@gmail.com>
2025-03-24 17:07:03 -0300

    ✨ feat: Relacionamento 1 para N Usuario x Pedidos

--- src/main/java/com/revisao/ecommerce/dto/PedidoDTO.java
+++ src/main/java/com/revisao/ecommerce/dto/PedidoDTO.java
@@ -2,8 +2,64 @@ package com.revisao.ecommerce.dto;
 
 import java.time.Instant;
 
+import com.revisao.ecommerce.entities.Pedido;
 import com.revisao.ecommerce.entities.StatusDoPedido;
 
-public record PedidoDTO(Instant momento, StatusDoPedido status) {
+public class PedidoDTO {
+
+	private Long id;
+	private Instant momento;
+	private StatusDoPedido status;
+	private Long clienteId;
+
+	public PedidoDTO() {
+
+	}
+
+	public PedidoDTO(Long id, Instant momento, StatusDoPedido status, Long clienteId) {
+		this.id = id;
+		this.momento = momento;
+		this.status = status;
+		this.clienteId = clienteId;
+	}
+
+	public PedidoDTO(Pedido pedido) {
+		this.id = pedido.getId();
+		this.momento = pedido.getMomento();
+		this.status = pedido.getStatus();
+		this.clienteId = pedido.getCliente().getId();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMomento() {
+		return momento;
+	}
+
+	public void setMomento(Instant momento) {
+		this.momento = momento;
+	}
+
+	public StatusDoPedido getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusDoPedido status) {
+		this.status = status;
+	}
+
+	public Long getClienteId() {
+		return clienteId;
+	}
+
+	public void setClienteid(Long clienteId) {
+		this.clienteId = clienteId;
+	}
 
 }
